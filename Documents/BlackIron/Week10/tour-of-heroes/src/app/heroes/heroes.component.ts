0b53cdca2597c3dab574f07f4540880d02162a53
Conner Gleason <connereg@optonline.net>
2022-08-26 14:30:10 -0400

    Working with subscriptions on observables

--- Documents/BlackIron/Week10/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Documents/BlackIron/Week10/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -24,7 +24,10 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(x => {
+      console.log(x);
+      this.heroes = x;
+    })
   }
 
 }
